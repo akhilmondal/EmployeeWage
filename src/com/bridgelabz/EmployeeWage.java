df0157ac86f7bdd9b3643fb33a4dcb2c333d0c8c
“akhilmondal” <akhilmandalfamily@gmail.com>
2023-01-11 23:25:16 +0530

    uc2_employee_wage for daily wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,15 +1,19 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         int employeeCheck = (int) (Math.random() * 10) % 2;
         System.out.println(employeeCheck);
-        if (employeeCheck == 0) {
+        int dailyWage = 0;
+        if (employeeCheck == 1) {
+            System.out.println("Employee is present.");
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        } else {
             System.out.println("Employee is absent.");
         }
-        else {
-            System.out.println("Employee is present.");
-        }
+        System.out.println("Daily wage of the employee is:  " +dailyWage);
     }
 }
