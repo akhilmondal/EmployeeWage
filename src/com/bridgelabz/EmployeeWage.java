8223a62ee729a260ad872362ebfe3dd84c180e11
“akhilmondal” <akhilmandalfamily@gmail.com>
2023-01-12 06:41:37 +0530

    uc6_employee_wage for 100 hours and for 20 days

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -8,31 +8,35 @@ public class EmployeeWage {
     static final int IS_FULL_TIME = 1;
     static final int IS_ABSENT = 0;
     static final int WORKING_DAYS_PER_MONTH = 20;
+    static final int WORKING_HOURS_PER_MONTH = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         int dayCount = 1;
         int dailyWage = 0;
-        while (dayCount <= WORKING_DAYS_PER_MONTH) {
+        int workingHour = 0;
+        while (dayCount <= WORKING_DAYS_PER_MONTH && workingHour < WORKING_HOURS_PER_MONTH) {
             int employeeCheck = (int) (Math.random() * 10) % 3;
             System.out.println(employeeCheck);
             switch (employeeCheck) {
                 case IS_PART_TIME:
                     System.out.println("Employee is Part Time");
                     dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    workingHour = workingHour + PART_TIME_HOUR;
                     break;
                 case IS_FULL_TIME:
                     System.out.println("Employee is Full Time");
                     dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    workingHour = workingHour + FULL_DAY_HOUR;
                     break;
                 default:
                     System.out.println("Employee is Absent");
                     dailyWage = WAGE_PER_HOUR * IS_ABSENT;
+                    workingHour = workingHour + IS_ABSENT;
             }
             System.out.println("Daily wage of Day => " + dayCount + " is "  + dailyWage);
+            System.out.println("Working hours is: " +workingHour);
             dayCount++;
         }
-
-
     }
 }
