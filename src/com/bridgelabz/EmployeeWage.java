daa8733a969ba1c3a982dd5f3149807b31e2a196
“akhilmondal” <akhilmandalfamily@gmail.com>
2023-01-18 22:27:15 +0530

    Refactor: A class level method is used to calculate Employee Wages.

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_TIME_HOUR = 7;
@@ -10,10 +11,9 @@ public class EmployeeWage {
     static final int WORKING_DAYS_PER_MONTH = 20;
     static final int WORKING_HOURS_PER_MONTH = 100;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
+    static void calculateWage() { //Calculating Wage inside the class level method.
         int dayCount = 1;
-        int dailyWage = 0;
+        int dailyWage;
         int workingHour = 0;
         while (dayCount <= WORKING_DAYS_PER_MONTH && workingHour < WORKING_HOURS_PER_MONTH) {
             int employeeCheck = (int) (Math.random() * 10) % 3;
@@ -34,9 +34,16 @@ public class EmployeeWage {
                     dailyWage = WAGE_PER_HOUR * IS_ABSENT;
                     workingHour = workingHour + IS_ABSENT;
             }
-            System.out.println("Daily wage of Day => " + dayCount + " is "  + dailyWage);
-            System.out.println("Working hours is: " +workingHour);
+            System.out.println("Daily wage of Day => " + dayCount + " is " + dailyWage);
+            System.out.println("Working hours is: " + workingHour);
             dayCount++;
         }
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation");
+        calculateWage(); // Calling the class level method inside the main method.
     }
 }
