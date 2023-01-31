4c362db27ddc37f98d1d570f30c67367d181b82a
“akhilmondal” <akhilmandalfamily@gmail.com>
2023-01-31 21:44:10 +0530

    Added: Program modified for checking daily wage of multiple companies.

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,48 +2,54 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 
-    static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_TIME_HOUR = 7;
     static final int IS_PART_TIME = 2;
     static final int IS_FULL_TIME = 1;
     static final int IS_ABSENT = 0;
-    static final int WORKING_DAYS_PER_MONTH = 20;
-    static final int WORKING_HOURS_PER_MONTH = 100;
-
-    static void calculateWage() { //Calculating Wage inside the class level method.
+    static int totalWage = 0;
+    /*
+    Declaring the method with three parameters.
+    So that we can calculate daily wages of multiple companies.
+     */
+    static void calculateWage(int wage, int workingDays, int workingHours) { //Calculating Wage inside the class level method.
         int dayCount = 1;
         int dailyWage;
         int workingHour = 0;
-        while (dayCount <= WORKING_DAYS_PER_MONTH && workingHour < WORKING_HOURS_PER_MONTH) {
+        while (dayCount <= workingDays && workingHour < workingHours) {
             int employeeCheck = (int) (Math.random() * 10) % 3;
             System.out.println(employeeCheck);
             switch (employeeCheck) {
                 case IS_PART_TIME:
                     System.out.println("Employee is Part Time");
-                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    dailyWage = wage * PART_TIME_HOUR;
                     workingHour = workingHour + PART_TIME_HOUR;
+                    totalWage = totalWage + dailyWage;
                     break;
                 case IS_FULL_TIME:
                     System.out.println("Employee is Full Time");
-                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    dailyWage = wage * FULL_DAY_HOUR;
                     workingHour = workingHour + FULL_DAY_HOUR;
+                    totalWage = totalWage + dailyWage;
                     break;
                 default:
                     System.out.println("Employee is Absent");
-                    dailyWage = WAGE_PER_HOUR * IS_ABSENT;
+                    dailyWage = wage * IS_ABSENT;
                     workingHour = workingHour + IS_ABSENT;
+                    totalWage = totalWage + dailyWage;
             }
             System.out.println("Daily wage of Day => " + dayCount + " is " + dailyWage);
             System.out.println("Working hours is: " + workingHour);
             dayCount++;
         }
+        System.out.println("Total wage of the company is: " +totalWage);
 
     }
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation");
-        calculateWage(); // Calling the class level method inside the main method.
+        calculateWage(200, 28, 200);
+        calculateWage(2000, 25, 300);// calculating daily wage of multiple company.
     }
 }
